377a1e4ba773634b3f7248434bdc14fe3c79222f
Daniel_Lepszy <daniel.lepszy@gmail.com>
2022-04-10 21:02:42 +0200

    Working on deserialization

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,74 @@
+package model;
+
+public class Product {
+
+    private int id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int category_id;
+    private String category_name;
+
+
+    //Used for GET
+    public Product(int id, String name, String description, double price,int category_id, String category_name) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category_id = category_id;
+        this.category_name = category_name;
+    }
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+}
--- src/test/java/training/APITests.java
+++ src/test/java/training/APITests.java
@@ -1,45 +1,62 @@
 package training;
 
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import io.restassured.response.ValidatableResponse;
+import model.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import training.util.RestAssuredUtil;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class APITests extends RestAssuredUtil {
 
     @BeforeAll
-    public static void setConfiguration(){
+    public static void setConfiguration() {
         String basePath = reader.readProperty("api", "BASE_PATH");
         setBasePath(basePath);
     }
+
     @Test
     public void getCategories() {
 
         String endpoint = "/category/read.php";
         ValidatableResponse response = given()
-        .when().get(endpoint).then();
+                .when().get(endpoint).then().assertThat().statusCode(200);
 
         response.log().body();
     }
 
     @Test
-    public void getSingleProduct() {
+    public void getDeserializedSingleProduct() {
         String endpoint = "/product/read_one.php";
-        ValidatableResponse response = given()
-                .queryParam("id", 3)
-                .when()
-                .get(endpoint)
-                .then();
 
-        response.log().body();
+        Product product = new Product(18,"Magnesium 250 mg (100 tablets)","A daily dose of our Multi-Vitamins fulfills a day’s nutritional needs for over 12 vitamins and minerals.",
+                10.00,4,"Supplements");
+//        ValidatableResponse response=
+        Product response =
+                given()
+                        .queryParam("id", "18")
+                        .when()
+                        .get(endpoint)
+                        .as(Product.class);
+        assertThat(response,samePropertyValuesAs(product));
     }
 
     @Test
     public void getCProducts() {
         String endpoint = "/product/read.php";
-        ValidatableResponse response = given().when().get(endpoint).then();
+
+        ValidatableResponse response = given().when().get(endpoint).then().log().headers()
+                .assertThat()
+                .statusCode(200)
+                .headers("Content-Type","application/json; charset=UTF-8")
+                .body("records.size()",greaterThan(0))
+                .body("records.id",everyItem(notNullValue()))
+                .body("records.id[0]",equalTo("18"));
 
         response.log().body();
 
