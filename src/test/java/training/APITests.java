7df45c51846c9a8b19a1da47e9a5f46813b313b4
Daniel_Lepszy <daniel.lepszy@gmail.com>
2022-04-11 23:45:04 +0200

    New API tests

--- src/test/java/training/APITests.java
+++ src/test/java/training/APITests.java
@@ -14,6 +14,9 @@ import static org.hamcrest.Matchers.*;
 
 public class APITests extends RestAssuredUtil {
 
+
+    private String base_path;
+
     @BeforeAll
     public static void setConfiguration() {
         String basePath = reader.readProperty("api", "BASE_PATH");
